4aa84df6f9163ae4d49043b436dbb9fd5e54ad5a
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2013-02-09 20:33:48 +0000

    copied

---
+++ sitemanager/FEfunction.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once("config.php");
+
+function createLinks() {
+    global $source;
+    $files = glob(BASE_PATH . "*");
+    $result = "";
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            $basename = basename($file);
+            $result.= "<a href=\"http://localhost/" . $basename . "\" target=\"_blank\">" . $basename . "</a></br>
+";
+        }
+    }
+    return $result;
+}
+
+function validateInput($input) {
+    if (($input == "")||(preg_match('/[A-Z]/', $input))) {
+        echo "input non valido";
+        return false;
+    }
+    return true;
+}
+
+function migrate($source, $newSite, $mysqlDatabaseName) {
+    $fileCloner = new WPMigrateFile(BASE_PATH . $source, BASE_PATH . $newSite);
+    $errorMsg = "";
+    if (!$fileCloner->cloneSite()) {
+        $errorMsg .= $fileCloner->errorMsg . "</br>";
+    }
+
+    $dbCloner = new DBCloner($mysqlDatabaseName, MYSQL_USER_NAME, MYSQL_PASSWORD, MYSQL_HOST, "db_" . $newSite,$source,$newSite);
+    if (!$dbCloner->migrate()) {
+        $errorMsg .= $dbCloner->errormsg . "</br>";
+    }
+    $fileCloner->changeWpconfig($mysqlDatabaseName, "db_" . $newSite);
+    return $errorMsg;
+}
+
+?>
