99ec6722aee2602eef70b037822e103ab5743a73
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2013-02-25 22:10:51 +0000

    rebased from branch

--- DBCloner.php
+++ DBCloner.php
@@ -12,6 +12,8 @@ class DBCloner {
     public $mysqlDatabaseNameNew;
     public $sourcename;
     public $destName;
+    private $con = null;
+    private $importFilename;
 
     function __construct($mysqlDatabaseName, $mysqlUserName, $mysqlPassword, $mysqlHostName, $mysqlDatabaseNameNew, $source, $dest) {
         $this->mysqlDatabaseName = $mysqlDatabaseName;
@@ -21,73 +23,86 @@ class DBCloner {
         $this->mysqlDatabaseNameNew = $mysqlDatabaseNameNew;
         $this->sourcename = $source;
         $this->destName = $dest;
+        $this->con = mysql_connect($this->mysqlHostName, $this->mysqlUserName, $this->mysqlPassword);
+    }
+
+    public function cleanAndClose() {
+        if (!unlink($this->mysqlImportFilename)) {
+            $this->errormsg .= "</br>Impossibile cancellare il file temporaneo " . $this->mysqlImportFilename;
+        }
+        if ($this->con != null) {
+            mysql_close($this->con);
+        }
+    }
+
+    public function mysqldumpOfDb($directory, $destFileName = null) {
+        if ($destFileName != null) {
+            $returnedFilename = $directory . DIRECTORY_SEPARATOR . $destFileName;
+        } else {
+            $returnedFilename = tempnam($directory, "");
+        }
+        $command = "\"" . MYSQL_BIN_BASE_PATH . "mysqldump\" --opt --host=" . $this->mysqlHostName . " --user=" . $this->mysqlUserName . " --password=" . $this->mysqlPassword . " " . $this->mysqlDatabaseName . " --single-transaction > \"" . $returnedFilename . "\" 2>&1";
+        exec($command, $output, $worked);
+        if ($worked == 1) {
+            $this->errormsg .= "Impossibile esportare il file " . $this->mysqlImportFilename . " sul DB " . mysql_error();
+            return false;
+        }
+        return $returnedFilename;
     }
 
     function migrate() {
-        $mysqlImportFilename = tempnam(__DIR__, "");
-        $command = "\"" . MYSQL_BIN_BASE_PATH . "mysqldump\" --opt --host=" . $this->mysqlHostName . " --user=" . $this->mysqlUserName . " --password=" . $this->mysqlPassword . " " . $this->mysqlDatabaseName . " --single-transaction > \"" . $mysqlImportFilename . "\" 2>&1";
-        exec($command, $output, $worked);
-        if ($worked == 1) {
-            $this->errormsg = "Impossibile esportare il file " . $mysqlImportFilename . " sul DB";
-            if (!unlink($mysqlImportFilename)) {
-                $this->errormsg .= "</br>Impossibile cancellare il file temporaneo " . $mysqlImportFilename;
-            }
-            return false;
-        }
-
-        $con = mysql_connect($this->mysqlHostName, $this->mysqlUserName, $this->mysqlPassword);
-        if (!$con) {
-            $this->errormsg = "Could not connect: " . mysql_error();
-            if (!unlink($mysqlImportFilename)) {
-                $this->errormsg .= "</br>Impossibile cancellare il file temporaneo " . $mysqlImportFilename;
-            }
-            return false;
-        }
-
-//        $sql = "DROP DATABASE " . $this->mysqlDatabaseNameNew. " IF EXISTS";
-//        if (!mysql_query($sql, $con)) {
-//            $this->errormsg = "Could not delete existent db " . $this->mysqlDatabaseNameNew;
-//            if (!unlink($mysqlImportFilename)) {
-//                $this->errormsg .= "</br>Impossibile cancellare il file temporaneo " . $mysqlImportFilename;
-//            }
+        $this->mysqlImportFilename = $this->mysqldumpOfDb(__DIR__);
+//        $command = "\"" . MYSQL_BIN_BASE_PATH . "mysqldump\" --opt --host=" . $this->mysqlHostName . " --user=" . $this->mysqlUserName . " --password=" . $this->mysqlPassword . " " . $this->mysqlDatabaseName . " --single-transaction > \"" . $this->mysqlImportFilename . "\" 2>&1";
+//        exec($command, $output, $worked);
+//        if ($worked == 1) {
+//            $this->errormsg .= "Impossibile esportare il file " . $this->mysqlImportFilename . " sul DB " . mysql_error();
 //            return false;
 //        }
-//       
+
         $sql = "CREATE DATABASE " . $this->mysqlDatabaseNameNew;
-        if (!mysql_query($sql, $con)) {
-            $this->errormsg = "Could not create db " . $this->mysqlDatabaseNameNew;
-            if (!unlink($mysqlImportFilename)) {
-                $this->errormsg .= "</br>Impossibile cancellare il file temporaneo " . $mysqlImportFilename;
-            }
+        if (!mysql_query($sql, $this->con)) {
+            $this->errormsg .= "Could not create db " . $this->mysqlDatabaseNameNew . " " . mysql_error();
             return false;
         }
 
         $sql = "USE " . $this->mysqlDatabaseNameNew;
-        if (!mysql_query($sql, $con)) {
-            $this->errormsg = "Could not select db " . $this->mysqlDatabaseNameNew;
-            if (!unlink($mysqlImportFilename)) {
-                $this->errormsg .= "</br>Impossibile cancellare il file temporaneo " . $mysqlImportFilename;
-            }
+        if (!mysql_query($sql, $this->con)) {
+            $this->errormsg .= "Could not select db " . $this->mysqlDatabaseNameNew . " " . mysql_error();
             return false;
         }
-        $content = file_get_contents($mysqlImportFilename);
-        $replaced = str_replace($this->mysqlDatabaseName, $this->mysqlDatabaseNameNew, $content);
-        $replaced = str_replace($this->sourcename, $this->destName, $replaced);
-        file_put_contents($mysqlImportFilename, $replaced);
-        $command = "\"" . MYSQL_BIN_BASE_PATH . "mysql\" --host=" . $this->mysqlHostName . " --user=" . $this->mysqlUserName . " --password=" . $this->mysqlPassword . " " . $this->mysqlDatabaseNameNew . " < \"" . $mysqlImportFilename . "\"";
+        $this->migrateDbFiles($this->mysqlImportFilename);
+        $command = "\"" . MYSQL_BIN_BASE_PATH . "mysql\" --host=" . $this->mysqlHostName . " --user=" . $this->mysqlUserName . " --password=" . $this->mysqlPassword . " " . $this->mysqlDatabaseNameNew . " < \"" . $this->mysqlImportFilename . "\"";
         @exec($command, $output = array(), $worked);
-        if (!unlink($mysqlImportFilename)) {
-            $this->errormsg = "Impossibile cancellare il file temporaneo " . $mysqlImportFilename;
-            return false;
-        }
         if ($worked == 1) {
-            $this->errormsg = "Impossibile importare il file " . $mysqlImportFilename . " sul DB";
-            if (!unlink($mysqlImportFilename)) {
-                $this->errormsg .= "</br>Impossibile cancellare il file temporaneo " . $mysqlImportFilename;
-            }
+            $this->errormsg .= "Impossibile importare il file " . $this->mysqlImportFilename . " sul DB";
             return false;
         }
         return true;
+    }
+
+    public function exportDbToPath($sqlPath,$source,$newConfig) {
+        $dumpFileOfDb = $this->mysqldumpOfDb(RELASE_BASE_PATH, $sqlPath);
+        $content = file_get_contents($dumpFileOfDb);
+        $replaced = str_replace("http://localhost/" . $source, "http://www." . $newConfig['domainName'] . "." . $newConfig['domain'], $content);
+        $replaced = str_replace("db_" . $source, $newConfig['newDb'], $replaced);
+        $replaced = str_replace($source, $newConfig['domainName'], $replaced);
+        $replaced = str_replace("localhost", "www." . $newConfig['domainName'] . "." . $newConfig['domain'], $replaced);
+        file_put_contents($dumpFileOfDb, $replaced);
+    }
+
+    public function migrateDbFiles($fileName) {
+        $content = file_get_contents($fileName);
+        $replaced = str_replace($this->sourcename, $this->destName, $content);
+        $replaced = str_replace($this->mysqlDatabaseName, $this->mysqlDatabaseNameNew, $replaced);
+        file_put_contents($fileName, $replaced);
+    }
+
+    private function cancellDB() {
+        $sql = "DROP DATABASE IF EXISTS " . $this->mysqlDatabaseNameNew;
+        if (!mysql_query($sql, $this->con)) {
+            $this->errormsg .= "Could not delete existent db " . $this->mysqlDatabaseNameNew . " " . mysql_error();
+            return false;
+        }
     }
 
 }
--- WPMigrateFile.php
+++ WPMigrateFile.php
@@ -17,7 +17,7 @@ class WPMigrateFile {
     }
 
     function cloneSite() {
-        $this->recurseCopy($this->sourcePath, $this->destPath);
+        return $this->recurseCopy($this->sourcePath, $this->destPath);
     }
 
     function recurseCopy($src, $dst) {
@@ -27,12 +27,12 @@ class WPMigrateFile {
             return false;
         }
         if (!is_dir($src)) {
-            $this->errorMsg = $src . " is not a dir";
+            $this->errorMsg .= $src . " is not a dir";
             return false;
         }
         $dir = opendir($src);
         if (!mkdir($dst)) {
-            $this->errorMsg = "Could not create dir " . $dst;
+            $this->errorMsg .= "Could not create dir " . $dst;
             return false;
         }
         while (false !== ( $file = readdir($dir))) {
@@ -41,7 +41,7 @@ class WPMigrateFile {
                     $this->recurseCopy($src . '/' . $file, $dst . '/' . $file);
                 } else {
                     if (!copy($src . '/' . $file, $dst . '/' . $file)) {
-                        $this->errorMsg = "Could not copy " . $src . '/' . $file;
+                        $this->errorMsg .= "Could not copy " . $src . '/' . $file;
                         return false;
                     }
                 }
@@ -56,14 +56,60 @@ class WPMigrateFile {
         $file = file_get_contents($wpConfigFile);
         if ($file) {
             $file = str_replace($searchFor, $replaceWith, $file);
+        } else {
+            $this->errorMsg .= $wpConfigFile . " non aperto<br>";
         }
         $fh = fopen($wpConfigFile, 'w');
-        fwrite($fh, $file);
+        if (fwrite($fh, $file) === false) {
+            $this->errorMsg .= "Cannot write to file ($wpConfigFile)<br>";
+        }
+        fclose($fh);
+    }
+
+    function switchConfigFile($fileToSwitchup, $backName = null) {
+        if (file_exists($this->destPath . "\\" . $fileToSwitchup) && is_file($this->destPath . "\\" . $fileToSwitchup)) {
+            if ($backName != null && $backName != "") {
+                $file = file_get_contents($this->destPath . "\wp-config.php");
+                $fh = fopen($this->destPath . "\\" . $backName, 'w');
+                if (fwrite($fh, $file) === false) {
+                    echo "Cannot write to file ($backName)<br>";
+                }
+                fclose($fh);
+            }
+            if (!rename($this->destPath . "\\" . $fileToSwitchup, $this->destPath . "\wp-config.php")) {
+                echo "Cannot switch to file (" . $fileToSwitchup . ")<br>";
+            }
+        }
+    }
+
+    function createReleaseConfigAndBckpLocal($newConfig) {
+        $sitenameOld = basename($this->sourcePath);
+        $wpConfigFile = $this->destPath . "\wp-config.php";
+        $wpConfigFileLocale = $this->destPath . "\wp-config-locale.php";
+        $file = file_get_contents($wpConfigFile);
+        $fh = fopen($wpConfigFileLocale, 'w');
+        if (fwrite($fh, $file) === false) {
+            $echo.= "Cannot write to file ($wpConfigFileLocale)<br>";
+        }
+        fclose($fh);
+        if ($file) {
+            $file = str_replace("define('DB_NAME', 'db_" . $sitenameOld . "');", "define('DB_NAME', '" . $newConfig['newDb'] . "');", $file);
+            $file = str_replace("define('DB_USER', '" . MYSQL_USER_NAME . "');", "define('DB_USER', '" . $newConfig['userName'] . "');", $file);
+            $file = str_replace("define('DB_PASSWORD', '" . MYSQL_PASSWORD . "');", "define('DB_PASSWORD', '" . $newConfig['password'] . "');", $file);
+            $file = str_replace("define('DB_HOST', '" . MYSQL_HOST . "');", "define('DB_HOST', '" . $newConfig['hostdb'] . "');", $file);
+        } else {
+            $echo = $wpConfigFile . " non aperto<br>";
+        }
+
+        $fh = fopen($wpConfigFile, 'w');
+        if (fwrite($fh, $file) === false) {
+            $echo.= "Cannot write to file ($wpConfigFile)<br>";
+        }
         fclose($fh);
     }
 
     function removeDir($dir) {
-        $this->errorMsg = "La pagina esiste già";
+        $this->errorMsg .= "La pagina esiste già";
         /*
           if (!file_exists($dir))
           return true;
