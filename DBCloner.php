ac74360fe34b2f0579fa8d7ddd985db7bac38f20
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2013-11-17 00:11:35 +0000

    commited from branch

--- DBCloner.php
+++ DBCloner.php
@@ -27,6 +27,7 @@ class DBCloner {
     }
 
     public function cleanAndClose() {
+        $this->fixSerializedData();
         if (!unlink($this->mysqlImportFilename)) {
             $this->errormsg .= "<br/>Impossibile cancellare il file temporaneo " . $this->mysqlImportFilename;
         }
@@ -35,15 +36,66 @@ class DBCloner {
         }
     }
 
+    function recursive_unserialize_replace($data) {
+        if (DEBUG) {
+            echo $data . "<br/><br/>
+";
+        }
+        if (is_string($data) && ( $unserialized = @unserialize($data) ) === false) {
+            $data = html_entity_decode($data, ENT_QUOTES, 'UTF-8');
+            $data = str_replace("\n", '', $data);
+            $data = str_replace("\r", '', $data);
+            $data = str_replace("\r\n", '', $data);
+            $data = preg_replace_callback('/s:(\d+):"(.*?)";/', function($match) {
+                        $temp = strlen($match[2]);
+                        $result = "s:" . $temp . ":\"" . $match[2] . "\";";
+                        return $result;
+                    }, $data);
+        }
+        $obj = unserialize($data);
+        return $data;
+    }
+
+    public function fixSerializedData() {
+        $update_sql = array();
+        $upd = false;
+        $query = "SELECT option_id,option_name,option_value FROM " . "db_" .$this->destName  . ".wp_options WHERE option_name IN ('arras_options','widget_text','WPS_setting','dashboard_widget_options','aio-favicon_settings')";
+        if (DEBUG) {
+            echo $query;
+        }
+        if (!($data = mysql_query($query, $this->con))) {
+            echo mysql_error();
+            return false;
+        }
+        while ($row = mysql_fetch_array($data)) {
+            $edited_data = $data_to_fix = $row['option_value'];
+            $edited_data = $this->recursive_unserialize_replace($data_to_fix);
+            // Something was changed
+            if ($edited_data != $data_to_fix) {
+                $update_sql[] = 'UPDATE ' . "db_" .$this->destName  . '.wp_options SET option_value = "' . mysql_real_escape_string($edited_data) . '" WHERE option_id = ' . $row['option_id'];
+                $upd = true;
+            }
+        }
+        if ($upd) {
+            foreach ($update_sql as $updQuery) {
+                $result = mysql_query($updQuery, $this->con);
+                echo ("Execute query");
+                if (!$result) {
+                    print_r(mysql_error());
+                }
+            }
+        }
+    }
+
     public function mysqldumpOfDb($directory, $destFileName = null) {
         if ($destFileName != null) {
-            $returnedFilename = $directory .$destFileName;
+            $returnedFilename = $directory . $destFileName;
         } else {
             $returnedFilename = tempnam($directory, "");
         }
         $command = "\"" . MYSQL_BIN_BASE_PATH . "mysqldump\" --host=" . $this->mysqlHostName . " --user=" . $this->mysqlUserName . " --password=" . $this->mysqlPassword . " " . $this->mysqlDatabaseName . " --single-transaction --opt > \"" . $returnedFilename . "\" 2>&1";
-        if (DEBUG){
-            echo "<br/>".$command;
+        if (DEBUG) {
+            echo "<br/>" . $command;
         }
         exec($command, $output, $worked);
         if ($worked == 1) {
@@ -57,10 +109,9 @@ class DBCloner {
         $this->mysqlImportFilename = $this->mysqldumpOfDb("tmp");
         $command = "\"" . MYSQL_BIN_BASE_PATH . "mysqldump\" --opt --host=" . $this->mysqlHostName . " --user=" . $this->mysqlUserName . " --password=" . $this->mysqlPassword . " " . $this->mysqlDatabaseName . " --single-transaction > \"" . $this->mysqlImportFilename . "\" 2>&1";
         exec($command, $output, $worked);
-        if(DEBUG){
-            echo "<br/>".$command;
+        if (DEBUG) {
+            echo "<br/>" . $command;
             print_r($output);
-            
         }
         if ($worked == 1) {
             $this->errormsg .= "<br/>Impossibile esportare il file " . $this->mysqlImportFilename . " sul DB " . mysql_error();
@@ -80,12 +131,12 @@ class DBCloner {
         }
         $this->migrateDbFiles($this->mysqlImportFilename);
         $command = "\"" . MYSQL_BIN_BASE_PATH . "mysql\" --host=" . $this->mysqlHostName . " --user=" . $this->mysqlUserName . " --password=" . $this->mysqlPassword . " " . $this->mysqlDatabaseNameNew . " < \"" . $this->mysqlImportFilename . "\"";
-        if (DEBUG){
+        if (DEBUG) {
+            echo $command . "</br>";
             @exec($command, $output = array(), $worked);
-        }else {
-            echo $command."</br>";
-            exec($command, $output = array(), $worked);
             print_r($output);
+        } else {
+            exec($command, $output = array(), $worked);
         }
         if ($worked == 1) {
             $this->errormsg .= "Impossibile importare il file " . $this->mysqlImportFilename . " sul DB";
@@ -94,20 +145,20 @@ class DBCloner {
         return true;
     }
 
-    public function exportDbToPath($sqlPath,$source,$newConfig) {
-        $dumpFileOfDb = $this->mysqldumpOfDb(BASE_PATH.$source.DIRECTORY_SEPARATOR, $sqlPath);
+    public function exportDbToPath($sqlPath, $source, $newConfig) {
+        $dumpFileOfDb = $this->mysqldumpOfDb(BASE_PATH . $source . DIRECTORY_SEPARATOR, $sqlPath);
         $content = file_get_contents($dumpFileOfDb);
 
         //to replace domain ref
         $replaced = str_replace("http://localhost/" . $source, "http://www." . $newConfig['domainName'] . "." . $newConfig['domain'], $content);
-        
+
         //to replace db ref
         $replaced = str_replace("db_" . $source, $newConfig['newDb'], $replaced);
-        
+
         //to replace something like C:/wamp/www/sitename with relative path
-        $replaced = str_replace(BASE_PATH."/" . $source,"", $replaced);
-        
-         //to replace last localhost ref
+        $replaced = str_replace(BASE_PATH . "/" . $source, "", $replaced);
+
+        //to replace last localhost ref
         $replaced = str_replace("localhost", "www." . $newConfig['domainName'] . "." . $newConfig['domain'], $replaced);
         file_put_contents($dumpFileOfDb, $replaced);
         return $dumpFileOfDb;
