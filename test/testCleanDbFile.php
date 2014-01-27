281184c9c9974a0d2a0730f685ac213ac69fa570
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2014-01-27 21:39:43 +0000

    fixed next gen option migration bug solve

---
+++ test/testCleanDbFile.php
@@ -0,0 +1,18 @@
+<?php
+include_once("../config.php");
+include_once("../DBCloner.php");
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$nomeSitoLocale = "ideagiardinosrlroma2";
+$nomesitoRemoto = "http://www.ideagiardinosrlroma.it";
+$nomeFile ="C:\Users\Miro\Documents\Desktop\db_ideagiardinosrlroma2.sql";
+
+$db = new DBCloner("db_" . $nomeSitoLocale, MYSQL_USER_NAME, MYSQL_PASSWORD, MYSQL_HOST, null, $nomeSitoLocale, $nomesitoRemoto);
+$db->migrateDbFiles($nomeFile,false);
+
+
+?>
