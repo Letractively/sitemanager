22dd90d288ed0df341e8a1823dec6b4a89a5e9b4
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2014-05-20 22:19:34 +0000

    added new test and svn wrapper

--- branches/remotealligner/test/testSvn.php
+++ branches/remotealligner/test/testSvn.php
@@ -1,37 +1,12 @@
 <?php
 
+//TODO: non funziona anche se sembra di si: forse per il processo in background??
+
 include_once("../config.php");
 include_once("../SubversionWrapper.php");
 
-$newSite = "paperino4";
-//
-//$useragent = "Mozilla Firefox";
-//$ch = curl_init();
-//$url = 'http://' . SVN_SERVER . '/create.php?r=' . $newSite;
-//curl_setopt($ch, CURLOPT_URL, $url);
-//curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($ch, CURLOPT_USERPWD, SVN_USER . ":" . SVN_PASSWORD);
-//curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-//echo curl_exec($ch);
-//curl_close($ch);
+$newSite = "uno";
 
-function esegui($command) {
-    echo $command."<br>";
-    echo shell_exec($command);
-//    
-//    if (substr(php_uname(), 0, 7) == "Windows") {
-//        $WshShell = new COM("WScript.Shell");
-//        $oExec = $WshShell->exec($command);
-//    } else {
-//        exec($command . " > /dev/null &", $output, $return_var);
-//    }
-}
-
-$command = "svn co http://" . SVN_SERVER . "/svn/" . $newSite . " " . BASE_PATH . $newSite;
-esegui($command);
-$command = "svn add --force " . BASE_PATH . $newSite . "\* --auto-props --parents --depth infinity -q";
-esegui($command);
-$command = "svn commit " . BASE_PATH . $newSite . " -m \"ciao\"";
-esegui($command);
+$svnCli = new SubversionWrapper($newSite, SVN_USER, SVN_PASSWORD);
+$svnCli->createRepo();
 ?>
