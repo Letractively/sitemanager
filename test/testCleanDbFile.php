7b733de997b92e8c0b07b54a94102dafbb2c8178
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2014-07-13 16:21:32 +0000

    u

--- test/testCleanDbFile.php
+++ test/testCleanDbFile.php
@@ -7,12 +7,12 @@ include_once("../DBCloner.php");
  * and open the template in the editor.
  */
 
-$nomeSitoLocale = "ideagiardinosrlroma2";
-$nomesitoRemoto = "http://www.meloshpub.it/testme";
-$nomeFile ="C:\Users\Miro\Documents\Desktop\db_ideagiardinosrlroma2.sql";
+$nomeSitoLocale = "master";
+$nomesitoRemoto = "http://localhost/test";
+$nomeFile ="C:\Users\Miro\Documents\Desktop\db_test.sql";
 
 $db = new DBCloner("db_" . $nomeSitoLocale, MYSQL_USER_NAME, MYSQL_PASSWORD, MYSQL_HOST, null, $nomeSitoLocale, $nomesitoRemoto);
-$db->migrateDbFiles($nomeFile,false);
+$db->migrateDbFiles($nomeFile);
 
 
 ?>
