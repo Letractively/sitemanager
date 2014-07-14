f6acb52d3ded1bf22684bbcd0c9ad6a2f4edd085
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2014-07-14 20:10:58 +0000

    added debug print function

--- SubversionWrapper.php
+++ SubversionWrapper.php
@@ -53,45 +53,69 @@ class SubversionWrapper {
     function committAll($message) {
         $command = "svn add --force " . BASE_PATH . $this->repos . "\* --auto-props --parents --depth infinity -q";
         $this->exec->execute($command, false);
-        if (DEBUG){
+        if (DEBUG) {
             echo "RETURN FROM ADD</br>";
-            print_r($this->exec->getStdOut());
+            if (!empty($this->exec->getStdOut())) {
+                echo "Stdout: </br>";
+                print_r($this->exec->getStdOut());
+            }
             echo "</br>";
-            print_r($this->exec->getStdErr());
-            echo "</br>Return code: ".$this->exec->getRetCode();
+            if (!empty($this->exec->getStdOut())) {
+                echo "StdErr: </br>";
+                print_r($this->exec->getStdErr());
+            }
+            echo "</br>Return code: " . $this->exec->getRetCode();
         }
         $command = "svn commit " . BASE_PATH . $this->repos . " -m \"" . $message . "\"";
         $this->exec->execute($command, true);
-        if (DEBUG){
+        if (DEBUG) {
             echo "RETURN FROM COMMIT</br>";
-            print_r($this->exec->getStdOut());
+            if (!empty($this->exec->getStdOut())) {
+                echo "Stdout: </br>";
+                print_r($this->exec->getStdOut());
+            }
             echo "</br>";
-            print_r($this->exec->getStdErr());
-            echo "</br>Return code: ".$this->exec->getRetCode();
+            if (!empty($this->exec->getStdOut())) {
+                echo "StdErr: </br>";
+                print_r($this->exec->getStdErr());
+            }
+            echo "</br>Return code: " . $this->exec->getRetCode();
         }
     }
 
     function updateAll() {
         $command = "svn update " . BASE_PATH . $this->repos;
         $this->exec->execute($command, false);
-        if (DEBUG){
+        if (DEBUG) {
             echo "RETURN FROM UPDATE</br>";
-            print_r($this->exec->getStdOut());
+            if (!empty($this->exec->getStdOut())) {
+                echo "Stdout: </br>";
+                print_r($this->exec->getStdOut());
+            }
             echo "</br>";
-            print_r($this->exec->getStdErr());
-            echo "</br>Return code: ".$this->exec->getRetCode();
+            if (!empty($this->exec->getStdOut())) {
+                echo "StdErr: </br>";
+                print_r($this->exec->getStdErr());
+            }
+            echo "</br>Return code: " . $this->exec->getRetCode();
         }
     }
 
     function checkout() {
         $command = "svn co http://" . SVN_SERVER . "/svn/" . $this->repos . " " . BASE_PATH . $this->repos . " --username " . SVN_USER . " --password " . SVN_PASSWORD;
         $this->exec->execute($command, false);
-        if (DEBUG){
+        if (DEBUG) {
             echo "RETURN FROM CHECKOUT</br>";
-            print_r($this->exec->getStdOut());
+            if (!empty($this->exec->getStdOut())) {
+                echo "Stdout: </br>";
+                print_r($this->exec->getStdOut());
+            }
             echo "</br>";
-            print_r($this->exec->getStdErr());
-            echo "</br>Return code: ".$this->exec->getRetCode();
+            if (!empty($this->exec->getStdOut())) {
+                echo "StdErr: </br>";
+                print_r($this->exec->getStdErr());
+            }
+            echo "</br>Return code: " . $this->exec->getRetCode();
         }
     }
 
@@ -134,7 +158,7 @@ class SubversionWrapper {
             echo $result . "\n</br>";
         }
         curl_close($ch);
-        $resAsArray = json_decode($result,true);
+        $resAsArray = json_decode($result, true);
         if ($resAsArray["return_var"] == 0 && isset($resAsArray["output"])) {
             return $resAsArray["output"];
         }
