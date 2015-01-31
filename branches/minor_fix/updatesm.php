c9b900a2ad006f56b3ffeded8cf8c33683c593d2
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2015-01-31 10:55:54 +0000

    adde function for autoinstall, fixed bug when end ftp transfer

--- branches/minor_fix/updatesm.php
+++ branches/minor_fix/updatesm.php
@@ -5,6 +5,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+include_once('SubversionWrapper.php');
+include_once('config.php');
+
 $svnSiteManager = new SubversionWrapper("https://sitemanager.googlecode.com/svn/trunk", "", "");
 $svnSiteManager->setRepos(__DIR__);
 $svnSiteManager->updateAll();
+header("Location: index.php");
