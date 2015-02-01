9049f40bce4ef76a9e75c795e1c8d918926a5287
ilpolemica <ilpolemica@4b58da8b-e26e-2733-61b7-768f3c8980d4>
2015-02-01 11:43:07 +0000

    added change for sql

--- updatesm.php
+++ updatesm.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,6 +11,22 @@ include_once('config.php');
 $svnSiteManager = new SubversionWrapper("https://sitemanager.googlecode.com/svn/trunk", "", "");
 $svnSiteManager->setRepos(basename(__DIR__));
 $svnSiteManager->updateAll();
-if (!$svnSiteManager->getHasError()){
-    header("Location: index.php");
+$sql = "DESCRIBE site_manager.sm_prodotti;";
+$db = new DBConfig(MYSQL_HOST, MYSQL_USER_NAME, MYSQL_PASSWORD);
+$con = $db->connect();
+$castresult = mysql_query($sql, $con) or die(mysql_error());
+while ($row = mysql_fetch_assoc($castresult)) {
+    if ($row['Field'] == 'nome') {
+        if ($row['Key'] != 'MUL') {
+            $sql = "ALTER TABLE `sm_prodotti` ADD INDEX ( `nome` ) ;
+ALTER TABLE `sm_prodotti` ADD UNIQUE (
+`nome`
+);";
+            mysql_query($sql, $con);            
+            break;
+        }
+    }
 }
+if (!$svnSiteManager->getHasError()) {
+    header("Location: index.php");
+}    
